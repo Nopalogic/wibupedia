645bc4fa0c94771de7eae6ea391ba21116b5711f
Nopalogic <naufaladhi02@gmail.com>
2024-07-16 00:14:19 +0700

    feat(navbar): add authentication and user dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,13 @@ import Searchbar from "./Searchbar";
 
 import { getAnimeResponse } from "@/utils/api";
 
+import { authUserSession } from "@/utils/auth";
+import UserDropdown from "./UserDropdown";
+
 export default async function Navbar() {
   const animeData = await getAnimeResponse("anime");
+
+  const user = await authUserSession();
 
   return (
     <header className="fixed top-0 z-50 w-full bg-slate-900 text-white">
@@ -19,11 +24,15 @@ export default async function Navbar() {
         </div>
 
         <div className="flex justify-end gap-2">
-          <Link href={"/auth/login"}>
-            <button className="rounded-md bg-orange-500 px-5 py-2 hover:bg-orange-600">
-              Sign In
-            </button>
-          </Link>
+          {user ? (
+            <UserDropdown user={user.name} img={user.image} />
+          ) : (
+            <Link href="/api/auth/signin">
+              <button className="rounded-md bg-orange-500 px-5 py-2 hover:bg-orange-600">
+                Sign In
+              </button>
+            </Link>
+          )}
         </div>
       </nav>
     </header>
---
+++ src/components/UserDropdown.jsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React from "react";
+import { createPopper } from "@popperjs/core";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function UserDropdown({ user, img }) {
+  const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
+
+  const btnDropdownRef = React.createRef();
+  const popoverDropdownRef = React.createRef();
+
+  const openDropdownPopover = () => {
+    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
+      placement: "bottom-end",
+    });
+    setDropdownPopoverShow(true);
+  };
+
+  const closeDropdownPopover = () => {
+    setDropdownPopoverShow(false);
+  };
+
+  return (
+    <>
+      <div
+        className="block cursor-pointer select-none"
+        ref={btnDropdownRef}
+        onClick={(e) => {
+          e.preventDefault();
+          dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
+        }}
+      >
+        <div className="flex items-center gap-2 text-white">
+          <Image
+            src={img}
+            alt="profile picture"
+            className="size-10 rounded-full"
+            width={250}
+            height={250}
+          />
+          {user}
+        </div>
+      </div>
+
+      <div
+        ref={popoverDropdownRef}
+        className={`${dropdownPopoverShow ? "block" : "hidden"} z-50 float-left min-w-48 list-none rounded bg-white py-2 text-left text-base shadow-lg`}
+      >
+        <Link
+          href="/dashboard"
+          className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-slate-700"
+        >
+          Dashboard
+        </Link>
+        <Link
+          href="/api/auth/signout"
+          className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-slate-700"
+        >
+          Sign Out
+        </Link>
+      </div>
+    </>
+  );
+}

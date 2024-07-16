0f77189f291544fc64a7a64232c6888fb5127b30
Nopalogic <naufaladhi02@gmail.com>
2024-07-17 04:13:44 +0700

    hotfix: Change font size

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -5,10 +5,10 @@ export default function AnimeCard({animeUrl, imgUrl, title, eps }) {
 	return (
 		<Link href={animeUrl} className="cursor-pointer">
 			<figure className="flex flex-col gap-2">
-				<Image src={imgUrl} width={174} height={174} alt="" className=" h-96 w-full rounded-xl" />
+				<Image src={imgUrl} width={174} height={174} alt="" className="h-96 w-full rounded-xl" />
 				<div className="flex flex-col">
-					<h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
-					{eps && <h2 className="text-12 truncate font-normal capitalize text-white-4">{`${eps} episode`}</h2>}
+					<h1 className="text-lg truncate font-bold text-white">{title}</h1>
+					{eps && <h2 className="text-base truncate font-normal capitalize text-white">{`${eps} episode`}</h2>}
 				</div>
 			</figure>
 		</Link>
